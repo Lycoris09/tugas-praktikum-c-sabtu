c0874f73557f33347f8570e0c2ba6782fa676ed4
Lycoris09 <andirostiandi08@gmail.com>
2024-09-25 16:08:30 +0700

    first commit

---
+++ tugas-1/tugas-mandiri-1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+	printf(" Nama    :Andi Rostiandi\n");
+	printf(" Nim     :2441054 \n ");
+    printf("Jurusan :Teknik Informatika");
+}
+
---
+++ tugas-1/tugas-mandiri-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int p  =10;
+	int lb =7;
+	int luas=p*lb;
+	printf("Menghitung luas persegi \n");
+	printf("Rumus      = Panjang x Lebar \n");
+    printf("panjang    = 10cm \n");
+    printf("lebar      = 7cm \n");
+    printf("luas       = %i \n",luas);
+}
+
